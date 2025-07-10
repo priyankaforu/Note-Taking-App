9ecbf387ac3cffbbc59dd54b8cf4e6e66eea18bd
Priyanka Pudi <priyankapudi4u@gmail.com>
2025-07-10 17:42:44 +0530

    prepared for deployment

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,25 +1,29 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import path from "path";
 
-
-import notesRoutes from "./routes/notesRoutes.js"
-import { connectDB } from './config/db.js';
-import ratelimiter from './middleware/ratelimiter.js';
-
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import ratelimiter from "./middleware/ratelimiter.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
-
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 
 // Body Parser middleware
 app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:5173"
-}));
+
 app.use(ratelimiter);
 
 // app.use((req,res,next) => {
@@ -27,14 +31,19 @@ app.use(ratelimiter);
 //   next();
 // });
 
-
 app.use("/api/notes", notesRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
 // Once DB connected, then only goahead and listen
-connectDB().then(()=>{
+connectDB().then(() => {
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
 });
-
-
